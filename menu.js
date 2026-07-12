5ffa19a5f270895d2348defb8ab47f6503bc3976
Fede <fedelaulhe@gmail.com>
2026-07-12 19:56:12 -0300

    entrega del portafolio

---
+++ menu.js
@@ -0,0 +1,30 @@
+console.log('Estoy enlazando javascript con HTML')
+console.log('Accediendo al formulario')
+
+const menuBtn = document.querySelector('#menu-btn')
+const navLinks = document.querySelector('.nav-links')
+
+menuBtn.addEventListener('click', () => {
+  navLinks.classList.toggle('abierto')
+})
+
+const btnDesplegar = document.querySelector('#btn-desplegar-contacto')
+const formularioContacto = document.querySelector('#form-contacto')
+
+btnDesplegar.addEventListener('click', () => {
+  formularioContacto.classList.toggle('mostrar')
+})
+
+// --- VALIDACIÓN DEL FORMULARIO ---
+const form = document.querySelector('#form-contacto')
+const email = document.querySelector('#email')
+const errorEmail = document.querySelector('#error-email')
+
+form.addEventListener('submit', (evento) => {
+  errorEmail.textContent = ''
+
+  if (!email.value.includes('@')) {
+      evento.preventDefault() 
+      errorEmail.textContent = 'Escribe un email válido'
+  }
+})
